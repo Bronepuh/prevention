eb61263f052104ba69d7dc41a04618c415b140c8
Bronepuh <darckday2007@mail.ru>
2025-03-17 23:51:52 +0300

    правки

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: process.env.VITE_BASE_URL || "/",
   plugins: [react()],
   resolve: {
     alias: {
